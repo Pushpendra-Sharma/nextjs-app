7a46ae57409d9e9c69f3f8ee612bc0ed8cccf718
Pushpendra Sharma <pushpendra-sharma@hotmail.com>
2023-04-18 16:29:28 +0530

    feat: added error validation in name input

--- containers/NameInputContainer.tsx
+++ containers/NameInputContainer.tsx
@@ -1,5 +1,5 @@
-import { useContext, useState } from 'react';
-import { Button, TextInput } from '@/components';
+import { ChangeEventHandler, useContext, useEffect, useState } from 'react';
+import { Button, Error, TextInput } from '@/components';
 import { AppContext } from '@/contexts/app-context';
 import { QuestionType } from '@/types';
 
@@ -8,23 +8,37 @@ type Props = {
 };
 
 export function NameInputContainer({ type }: Props) {
-  const { questionId, setQuestionId, dispatch } = useContext(AppContext);
+  const { questionId, setQuestionId, dispatch, responses } =
+    useContext(AppContext);
 
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState(responses[type]);
+  const [error, setError] = useState(false);
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = e => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    if (!value) setError(true);
+    else setError(false);
+  }, [value]);
 
   const handleClick = () => {
-    setQuestionId({
-      prev: Number(questionId.prev) + 1,
-      current: questionId.current + 1,
-      next: questionId.next + 1,
-    });
-    dispatch({ type: `SET_${type.toUpperCase()}`, payload: value });
+    if (value) {
+      setQuestionId({
+        prev: Number(questionId.prev) + 1,
+        current: questionId.current + 1,
+        next: questionId.next + 1,
+      });
+      dispatch({ type: `SET_${type.toUpperCase()}`, payload: value });
+    } else setError(true);
   };
 
   return (
-    <>
-      <TextInput value={value} handleChange={e => setValue(e.target.value)} />
+    <div className='flex flex-col gap-4'>
+      <TextInput value={value} handleChange={handleChange} />
+      {error && <Error message='Please fill this in' />}
       <Button label='OK' onClick={handleClick} />
-    </>
+    </div>
   );
 }
